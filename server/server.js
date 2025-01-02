d58281a7d1719c92db89a79146e24d67d2a470f9
ZoTaD <grundynicolas021@gmail.com>
2025-01-02 18:48:01 +0000

    configuracion de cors

--- server/server.js
+++ server/server.js
@@ -26,15 +26,14 @@ app.use(express.json());
 
 // Configuración de CORS
 app.use(cors({
-    origin: ['https://blomernapp.netlify.app'], // Reemplaza con la URL correcta de tu frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    // credentials: true,
+    origin: 'https://blomernapp.netlify.app', // URL exacta de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    credentials: true, // Permitir cookies o encabezados de autorización
 }));
 
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    console.log('Headers:', req.headers);
+    console.log('CORS Middleware ejecutado para:', req.method, req.url);
     next();
 });
 
