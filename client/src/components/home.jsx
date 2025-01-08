5fc6eda86bc37241acffd640567942593b21e403
ZoTaD <grundynicolas021@gmail.com>
2025-01-08 22:22:59 +0000

    configuracion de login.jsx

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -43,7 +43,7 @@ function Home() {
         <Container fluid style={{ minHeight: '100vh', backgroundColor: '#f8f9fa', paddingTop: '20px' }}>
             <Row className="align-items-center mb-4">
                 <Col>
-                    <h1 className="text-center">Bienvenido, {name}</h1>
+                    <h1 className="text-left">Bienvenido, {name}</h1>
                 </Col>
                 <Col xs="auto" className="text-end">
                     <Button variant="danger" onClick={handleLogout}>
