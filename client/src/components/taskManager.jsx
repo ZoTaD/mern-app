e847fd48bb4193108ab8be162ebc864ffb90e5be
ZoTaD <grundynicolas021@gmail.com>
2025-01-09 18:29:25 +0000

    configuracion de login.jsx

--- client/src/components/taskManager.jsx
+++ client/src/components/taskManager.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks, createTask, updateTask, deleteTask } from '../store/taskSlice';
 import { Card, Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 // import { data } from 'react-router-dom';
 
 function TaskManager() {
@@ -19,6 +20,8 @@ function TaskManager() {
         const options = { day: '2-digit', month: '2-digit', year: '2-digit' };
         return new Date(dateString).toLocaleDateString('es-ES', options); // Formato DD/MM/AA
     };
+
+
 
     // Crear
     const handleCreateTask = (e) => {
@@ -55,179 +58,163 @@ function TaskManager() {
         setNewTask({ title: task.title, description: task.description, status: task.status, });
     };
 
-    return (
-        <Container>
-            <Row>
+    const handleDragEnd = (result) => {
+        const { source, destination } = result;
 
-                <Col>
-                    <h2 className="text-center mb-4">Gestión de tareas</h2>
-                    <Form onSubmit={editingTask ? handleUpdateTask : handleCreateTask}>
-                        <Row>
-                            <Col md={3}>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Título"
-                                    value={newTask.title}
-                                    onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-                                    className="mb-2"
-                                />
-                            </Col>
-                            <Col md={3}>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Descripción"
-                                    value={newTask.description}
-                                    onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
-                                    className="mb-2"
-                                />
-                            </Col>
-                            <Col md={3}>
-                                <Form.Select
-                                    value={newTask.status}
-                                    onChange={(e) => setNewTask({ ...newTask, status: e.target.value })}
-                                    className="mb-2"
-                                >
-                                    <option value="Pendiente">Pendiente</option>
-                                    <option value="En Progreso">En Progreso</option>
-                                    <option value="Completada">Completada</option>
-                                </Form.Select>
-                            </Col>
-                            <Col md="auto">
-                                <Button type="submit" variant="primary" className="mb-2">
-                                    {editingTask ? 'Actualizar' : 'Agregar'}
-                                </Button>
-                                {editingTask && (
-                                    <Button
-                                        variant="secondary"
-                                        onClick={() => setEditingTask(null)}
-                                        className="mb-2 ms-2"
+        if (!destination) return;
+
+        if (source.droppableId !== destination.droppableId) {
+            const taskId = groupedTasks[source.droppableId][source.index]._id;
+
+            const updatedTask = {
+                ...groupedTasks[source.droppableId][source.index],
+                status: destination.droppableId,
+            };
+
+            dispatch(updateTask({ id: taskId, data: updatedTask }));
+        }
+    };
+
+    // Agrupar tareas por su estado
+    const groupedTasks = {
+        Pendiente: tasks.filter((task) => task.status === 'Pendiente'),
+        'En Progreso': tasks.filter((task) => task.status === 'En Progreso'),
+        Completada: tasks.filter((task) => task.status === 'Completada'),
+    };
+
+
+    return (
+        <DragDropContext onDragEnd={handleDragEnd}>
+            <Container>
+                <Row>
+                    <Col>
+                        <h2 className="text-center mb-4">Gestión de tareas</h2>
+                        <Form onSubmit={editingTask ? handleUpdateTask : handleCreateTask}>
+                            <Row>
+                                <Col md={3}>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Título"
+                                        value={newTask.title}
+                                        onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+                                        className="mb-2"
+                                    />
+                                </Col>
+                                <Col md={3}>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Descripción"
+                                        value={newTask.description}
+                                        onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+                                        className="mb-2"
+                                    />
+                                </Col>
+                                <Col md={3}>
+                                    <Form.Select
+                                        value={newTask.status}
+                                        onChange={(e) => setNewTask({ ...newTask, status: e.target.value })}
+                                        className="mb-2"
                                     >
-                                        Cancelar
+                                        <option value="Pendiente">Pendiente</option>
+                                        <option value="En Progreso">En Progreso</option>
+                                        <option value="Completada">Completada</option>
+                                    </Form.Select>
+                                </Col>
+                                <Col md="auto">
+                                    <Button type="submit" variant="primary" className="mb-2">
+                                        {editingTask ? 'Actualizar' : 'Agregar'}
                                     </Button>
-                                )}
-                            </Col>
-                        </Row>
-                    </Form>
-                </Col>
-            </Row>
-            <Row>
-                {/* Columna de tareas pendientes */}
-                <Col md={4}>
-                    <h3 className="text-center">Pendiente</h3>
-                    {tasks
-                        .filter((task) => task.status === 'Pendiente')
-                        .map((task, index) => (
-                            <Card key={task._id} className="mb-3">
-                                <Card.Body>
-                                    <div className="d-flex justify-content-between align-items-center mb-3">
-                                        <div>
-                                            <h6 className="text-muted">#{index + 1}</h6>
-                                            <h5>{task.title}</h5>
+                                    {editingTask && (
+                                        <Button
+                                            variant="secondary"
+                                            onClick={() => setEditingTask(null)}
+                                            className="mb-2 ms-2"
+                                        >
+                                            Cancelar
+                                        </Button>
+                                    )}
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row>
+                    {['Pendiente', 'En Progreso', 'Completada'].map((status) => (
+                        <Col md={4} key={status}>
+                            <div
+                                className={`column ${status === 'Pendiente'
+                                    ? 'pending-column'
+                                    : status === 'En Progreso'
+                                        ? 'in-progress-column'
+                                        : 'completed-column'
+                                    }`}
+                            >
+                                <h3 className="text-center">{status}</h3>
+                                <Droppable droppableId={status}>
+                                    {(provided) => (
+                                        <div
+                                            {...provided.droppableProps}
+                                            ref={provided.innerRef}
+                                            className="droppable-column"
+                                        >
+                                            {groupedTasks[status].map((task, index) => (
+                                                <Draggable
+                                                    key={task._id}
+                                                    draggableId={task._id}
+                                                    index={index}
+                                                >
+                                                    {(provided) => (
+                                                        <Card
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            className="mb-3"
+                                                        >
+                                                            <Card.Body>
+                                                                <div className="d-flex justify-content-between align-items-center mb-3">
+                                                                    <div>
+                                                                        <h6 className="text-muted">#{index + 1}</h6>
+                                                                        <h5>{task.title}</h5>
+                                                                    </div>
+                                                                </div>
+                                                                <p>{task.description}</p>
+                                                                <small className="text-muted">
+                                                                    Creado el: {formatDate(task.createdAt)}
+                                                                </small>
+                                                            </Card.Body>
+                                                            <Card.Footer className="d-flex justify-content-between">
+                                                                <Button
+                                                                    variant="warning"
+                                                                    size="sm"
+                                                                    onClick={() => handleEditClick(task)}
+                                                                >
+                                                                    Editar
+                                                                </Button>
+                                                                <Button
+                                                                    variant="danger"
+                                                                    size="sm"
+                                                                    onClick={() => handleDeleteTask(task._id)}
+                                                                >
+                                                                    Eliminar
+                                                                </Button>
+                                                            </Card.Footer>
+                                                        </Card>
+                                                    )}
+                                                </Draggable>
+                                            ))}
+                                            {provided.placeholder}
                                         </div>
-                                    </div>
-                                    <p>{task.description}</p>
-                                    <small className="text-muted">
-                                        Creado el: {formatDate(task.createdAt)}
-                                    </small>
-                                </Card.Body>
-                                <Card.Footer className="d-flex justify-content-between">
-                                    <Button
-                                        variant="warning"
-                                        size="sm"
-                                        onClick={() => handleEditClick(task)}
-                                    >
-                                        Editar
-                                    </Button>
-                                    <Button
-                                        variant="danger"
-                                        size="sm"
-                                        onClick={() => handleDeleteTask(task._id)}
-                                    >
-                                        Eliminar
-                                    </Button>
-                                </Card.Footer>
-                            </Card>
-                        ))}
-                </Col>
-                {/* Columna de tareas en progreso */}
-                <Col md={4}>
-                    <h3 className="text-center">En Progreso</h3>
-                    {tasks
-                        .filter((task) => task.status === 'En Progreso')
-                        .map((task, index) => (
-                            <Card key={task._id} className="mb-3">
-                                <Card.Body>
-                                    <div className="d-flex justify-content-between align-items-center mb-3">
-                                        <div>
-                                            <h6 className="text-muted">#{index + 1}</h6>
-                                            <h5>{task.title}</h5>
-                                        </div>
-                                    </div>
-                                    <p>{task.description}</p>
-                                    <small className="text-muted">
-                                        Creado el: {formatDate(task.createdAt)}
-                                    </small>
-                                </Card.Body>
-                                <Card.Footer className="d-flex justify-content-between">
-                                    <Button
-                                        variant="warning"
-                                        size="sm"
-                                        onClick={() => handleEditClick(task)}
-                                    >
-                                        Editar
-                                    </Button>
-                                    <Button
-                                        variant="danger"
-                                        size="sm"
-                                        onClick={() => handleDeleteTask(task._id)}
-                                    >
-                                        Eliminar
-                                    </Button>
-                                </Card.Footer>
-                            </Card>
-                        ))}
-                </Col>
-                {/* Columna de tareas completadas */}
-                <Col md={4}>
-                    <h3 className="text-center">Completada</h3>
-                    {tasks
-                        .filter((task) => task.status === 'Completada')
-                        .map((task, index) => (
-                            <Card key={task._id} className="mb-3">
-                                <Card.Body>
-                                    <div className="d-flex justify-content-between align-items-center mb-3">
-                                        <div>
-                                            <h6 className="text-muted">#{index + 1}</h6>
-                                            <h5>{task.title}</h5>
-                                        </div>
-                                    </div>
-                                    <p>{task.description}</p>
-                                    <small className="text-muted">
-                                        Creado el: {formatDate(task.createdAt)}
-                                    </small>
-                                </Card.Body>
-                                <Card.Footer className="d-flex justify-content-between">
-                                    <Button
-                                        variant="warning"
-                                        size="sm"
-                                        onClick={() => handleEditClick(task)}
-                                    >
-                                        Editar
-                                    </Button>
-                                    <Button
-                                        variant="danger"
-                                        size="sm"
-                                        onClick={() => handleDeleteTask(task._id)}
-                                    >
-                                        Eliminar
-                                    </Button>
-                                </Card.Footer>
-                            </Card>
-                        ))}
-                </Col>
-            </Row>
-        </Container>
+                                    )}
+                                </Droppable>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </DragDropContext>
     );
+
+
 
 }
 
