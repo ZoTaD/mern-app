837570a7ded6a10c3dd98d58b3288d8d35b354b5
ZoTaD <grundynicolas021@gmail.com>
2025-01-20 20:32:35 +0000

    Creacion de los test

--- client/src/components/taskManager.jsx
+++ client/src/components/taskManager.jsx
@@ -159,7 +159,7 @@ function TaskManager() {
                         </Form>
                     </Col>
                 </Row>
-                <Row className="custom-row" >
+                <Row className="no-wrap-row" >
                     {['Pendiente', 'En Progreso', 'Completada'].map((status) => (
                         <Col
                             md={4}
